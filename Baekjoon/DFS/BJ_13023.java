44b4607ae1b7819e4c5bf81201d284557ab1f56c
Park Min Sung <66161303+als9566@users.noreply.github.com>
2022-07-12 21:57:26 +0900

    [Baekjoon_13023] ABCDE

---
+++ Baekjoon/DFS/BJ_13023.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class BJ_13023 {
+	static boolean visited[];
+	static ArrayList<Integer>[] A;
+	static boolean arrive;
+	
+	public static void main(String[] args) {
+		int N,M;
+		arrive = false;
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		A = new ArrayList[N];
+		visited = new boolean[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < M; i++) {
+			int S = sc.nextInt();
+			int E = sc.nextInt();
+			A[S].add(E);
+			A[E].add(S);
+		}
+		for (int i = 0; i < N; i++) {
+			DFS(i, 1);     // depth 1부터 DFS 시작
+			if (arrive)
+				break;
+		}
+		if (arrive)
+			System.out.println("1");             
+		else  
+			System.out.println("0");             // 5의 깊이가 없다면 0출력
+	}
+	
+	public static void DFS(int now, int depth) { //DFS
+		if (depth == 5 || arrive) {              // depth가 5가 되면 만족이므로 종료
+			arrive = true;
+			return;
+		}
+		visited[now] = true;
+		for (int i : A[now]) {
+			if (!visited[i]) {
+				DFS(i, depth + 1);               // 재귀 호출이 될 때마다 depth++
+			}
+		}
+		visited[now] = false;
+	}
+
+}
